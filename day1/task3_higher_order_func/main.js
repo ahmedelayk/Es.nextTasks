abc088230eaed680b989aea9e8004fd403bd2729
ahmedelayk <ahmedelayek7@gmail.com>
2023-04-18 19:11:05 +0200

    day1

--- day1/task3_higher_order_func/main.js
+++ day1/task3_higher_order_func/main.js
@@ -1,17 +1,17 @@
-var fruits = ['apple','strawberry','banana','orange','mango'];
+var fruits = ['apple', 'strawberry', 'banana', 'orange', 'mango'];
 
 console.log(
-  fruits.every((fruit)=>typeof fruit === 'string')
+  fruits.every((fruit) => typeof fruit === 'string')
 )
 console.log(
-  fruits.some((fruit)=>fruit.startsWith('a'))
+  fruits.some((fruit) => fruit.startsWith('a'))
 )
-let newArr = fruits.filter((fruit)=>{
+let newArr = fruits.filter((fruit) => {
   return fruit.startsWith('b') || fruit.startsWith('s')
 })
 console.log(newArr)
 
-let likedFruits = fruits.map((fruit)=>`I like ${fruit}`)
+let likedFruits = fruits.map((fruit) => `I like ${fruit}`)
 // console.log(likedFruits)
 
-likedFruits.forEach((el)=>console.log(el))
+likedFruits.forEach((el) => console.log(el))
